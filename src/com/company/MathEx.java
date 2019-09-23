09d92cc5a4bb16978b8115da9041d7d970ee66f2
student <jessewang@Billericak12.com>
2019-09-23 12:29:12 -0400

    fwef

---
+++ src/com/company/MathEx.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class MathEx {
+
+    public static void main(String[] args) {
+
+    }
+	    public void swap(int x)
+        {
+            if (x>100)
+            {
+                int y=x%10;
+                int z=x%100;
+                System
+            }
+            int y = x%100;
+            int z = x%1000;
+
+
+
+
+        }
+}

6d5126bd4922bac8aef087e51828e1ccfb5b1a5b
student <jessewang@billericak12.com>
2019-09-27 11:11:28 -0400

    top

--- src/com/company/MathEx.java
+++ src/com/company/MathEx.java
@@ -1,16 +1,27 @@
+//Jesse Wang
+//MathEx
+//September 27th. 2019
+
+
 package com.company;
 
 public class MathEx {
 
     public static void main(String[] args)
     {
-        int x = swap(123);      //Integer Switch
+        int x = swap(123);              //Integer Switch
         System.out.println(x);
 
-        timeLeft(1, 34, 8, 20);                      //Train Time
+        timeLeft(1, 34, 8, 20);   //Train Time
 
-        dayOfWeek(0,22);                    //Calender
-        }
+        dayOfWeek(0,1);          //Calender
+        dayOfWeek(0,14);
+        dayOfWeek(6,22);
+        dayOfWeek(5,4);
+        dayOfWeek(1,24);
+        dayOfWeek(2,1);
+
+    }
 
         public static int swap(int x)
         {
@@ -38,15 +49,30 @@ public class MathEx {
         b = (hours2 + depMin) - (hours1 + curMin);
         c = b / 60;
         e = b % 60;
-        System.out.print(c + " hours " + e + " mins ");
+        System.out.println(c + " hours " + e + " mins ");
         return 0;
 
         }
 
         public static int dayOfWeek(int day, int date)
         {
-            int a = (date%7) - 1 + day;
-
+            int a = day + date - 1;     //gives the integer a, which is the total number of days past plus how far into the week we already in accordance to the 1st day// ; the minus one accounts for Sunday being equal to 0
+            a = a%7;                    //divided by 7 to get remainder. the rest are full weeks so they don't matter
+            System.out.println(a);
+            return 0;
         }
 
 }
+
+/*
+//132
+//6 hours 46 mins
+//0
+//6
+//6
+//1
+//3
+//2
+//
+//Process finished with exit code 0
+ */
